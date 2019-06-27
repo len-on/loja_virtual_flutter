af1761caf7c448b545437658b0b99c13bccb4ed0
len-on <assalinlenon@gmail.com>
2019-06-27 08:45:13 -0300

    Criação ta tela carrinho

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -4,6 +4,7 @@ import 'package:loja_virtual_flutter/datas/product_data.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:loja_virtual_flutter/models/cart_model.dart';
 import 'package:loja_virtual_flutter/models/user_model.dart';
+import 'package:loja_virtual_flutter/screens/cart_screen.dart';
 import 'package:loja_virtual_flutter/screens/login_screen.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -121,6 +122,10 @@ class _ProductScreenState extends State<ProductScreen> {
                         cartProduct.category = product.category;
 
                         CartModel.of(context).addCartItem(cartProduct);
+
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context)=>CartScreen())
+                        );
                       }else{
                         Navigator.of(context).push(
                           MaterialPageRoute(builder: (context)=>LoginScreen())
